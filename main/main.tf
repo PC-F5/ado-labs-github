5435ba976cba06888dc6872d9118f555a7937feb
Peter Colley <Peter.Colley@vigilant.it>
2023-08-17 15:13:59 +1000

    updated vnet terraform code

--- main/main.tf
+++ main/main.tf
@@ -12,69 +12,45 @@ resource "random_integer" "name_suffix" {
 
 # RESOURCE GROUP #
 
-resource "azurerm_resource_group" "vnet" {
+resource "azurerm_resource_group" "vnetRG" {
   name     = local.resource_group_name
   location = var.location
 }
+
 
 # VNET #
 
 resource "azurerm_network_security_group" "nsg1" {
   location            = var.vnet_location
   name                = local.network_security_group_name
-  resource_group_name = azurerm_resource_group.vnet.name
+  resource_group_name = azurerm_resource_group.vnetRG.name
 }
 
 resource "azurerm_route_table" "rt1" {
   location            = var.vnet_location
   name                = local.route_table_name
-  resource_group_name = azurerm_resource_group.vnet.name
+  resource_group_name = azurerm_resource_group.vnetRG.name
 }
 
-module "vnet" {
-  source              = "../../"
-  resource_group_name = azurerm_resource_group.vnet.name
-  use_for_each        = var.use_for_each
-  address_space       = ["10.0.0.0/16"]
-  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  subnet_names        = ["subnet1", "subnet2", "subnet3"]
-  vnet_location       = var.vnet_location
 
-  nsg_ids = {
-    subnet1 = azurerm_network_security_group.nsg1.id
-  }
+resource "azurerm_virtual_network" "vnet" {
+  name                = "test-vnet"
+  address_space       = ["10.21.21.0/24"]  # Set your desired address space
+  location            = azurerm_resource_group.vnetRG.location
+  resource_group_name = azurerm_resource_group.vnetRG.name
+}
 
-  subnet_service_endpoints = {
-    subnet2 = ["Microsoft.Storage", "Microsoft.Sql"],
-    subnet3 = ["Microsoft.AzureActiveDirectory"]
-  }
 
-  subnet_delegation = {
-    subnet2 = {
-      "Microsoft.Sql.managedInstances" = {
-        service_name = "Microsoft.Sql/managedInstances"
-        service_actions = [
-          "Microsoft.Network/virtualNetworks/subnets/join/action",
-          "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action",
-          "Microsoft.Network/virtualNetworks/subnets/unprepareNetworkPolicies/action",
-        ]
-      }
-    }
-  }
+resource "azurerm_subnet" "subnet1" {
+  name                 = "subnet1"
+  resource_group_name = azurerm_resource_group.vnetRG.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.21.22.0/24"]  # Set your desired subnet address range
+}
 
-  route_tables_ids = {
-    subnet1 = azurerm_route_table.rt1.id
-  }
-
-  tags = {
-    for tag in var.tags : tag => tag
-  }
-  
-  subnet_enforce_private_link_endpoint_network_policies = {
-    subnet2 = true
-  }
-
-  subnet_enforce_private_link_service_network_policies = {
-    subnet3 = true
-  }
+resource "azurerm_subnet" "subnet2" {
+  name                 = "subnet2"
+  resource_group_name = azurerm_resource_group.vnetRG.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.21.23.0/24"]  # Set your desired subnet address range
 }
