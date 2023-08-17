d0084fd79eab77a92288e875bdddbea6970785af
Peter Colley <Peter.Colley@vigilant.it>
2023-08-17 15:43:34 +1000

    added tags to fix null assignment error

--- main/main.tf
+++ main/main.tf
@@ -38,7 +38,7 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.21.21.0/24"]  # Set your desired address space
   location            = azurerm_resource_group.vnetRG.location
   resource_group_name = azurerm_resource_group.vnetRG.name
-  tags                = ""
+  tags                = ["peter.colley"]
 }
 
 
