024504d43841e0a2085f0898f6fddb90e695e569
Peter Colley <Peter.Colley@vigilant.it>
2023-08-18 12:18:10 +1000

    added public ip, load balancer and panos

--- main/main.tf
+++ main/main.tf
@@ -1,7 +1,9 @@
 locals {
-  resource_group_name = "${var.naming_prefix}-${random_integer.name_suffix.result}"
+  resource_group_name         = "${var.naming_prefix}-${random_integer.name_suffix.result}"
   network_security_group_name = "${var.naming_prefix}-${random_integer.name_suffix.result}"
-  route_table_name = "${var.naming_prefix}-${random_integer.name_suffix.result}"
+  route_table_name            = "${var.naming_prefix}-${random_integer.name_suffix.result}"
+  lb_name                     = "${var.naming_prefix}-${random_integer.name_suffix.result}"
+  pip_name                    = "${var.naming_prefix}-${random_integer.name_suffix.result}"
 }
 
 resource "random_integer" "name_suffix" {
@@ -12,48 +14,157 @@ resource "random_integer" "name_suffix" {
 
 # RESOURCE GROUP #
 
-resource "azurerm_resource_group" "vnetRG" {
+resource "azurerm_resource_group" "vnetrg" {
   name     = local.resource_group_name
   location = var.location
 }
 
 
-# VNET #
+# NETWORK SECURITY GROUP #
 
 resource "azurerm_network_security_group" "nsg1" {
-  location            = var.vnet_location
-  name                = local.network_security_group_name
-  resource_group_name = azurerm_resource_group.vnetRG.name
+  name                = "TestSecurityGroup1"
+  location            = azurerm_resource_group.vnetrg.location
+  resource_group_name = azurerm_resource_group.vnetrg.name
+
+  # security_rule {
+  #   name                       = "test123"
+  #   priority                   = 100
+  #   direction                  = "Inbound"
+  #   access                     = "Allow"
+  #   protocol                   = "Tcp"
+  #   source_port_range          = "*"
+  #   destination_port_range     = "*"
+  #   source_address_prefix      = "*"
+  #   destination_address_prefix = "*"
+  # }
+
+  tags = {
+    environment = "Test"
+  }
 }
+
+
+# USER DEFINED ROUTES #
 
 resource "azurerm_route_table" "rt1" {
-  location            = var.vnet_location
-  name                = local.route_table_name
-  resource_group_name = azurerm_resource_group.vnetRG.name
+  name                          = "TestRouteTable"
+  location                      = azurerm_resource_group.vnetrg.location
+  resource_group_name           = azurerm_resource_group.vnetrg.name
+  disable_bgp_route_propagation = false
+
+  # route {
+  #   name           = "route1"
+  #   address_prefix = "10.1.0.0/16"
+  #   next_hop_type  = "VnetLocal"
+  # }
+
+  tags = {
+    environment = "Test"
+  }
 }
 
 
+# VNET #
+
 resource "azurerm_virtual_network" "vnet" {
-  name                = "test-vnet"
+  name                = "TestVnet"
   address_space       = ["10.21.21.0/24"]  # Set your desired address space
-  location            = azurerm_resource_group.vnetRG.location
-  resource_group_name = azurerm_resource_group.vnetRG.name
+  location            = azurerm_resource_group.vnetrg.location
+  resource_group_name = azurerm_resource_group.vnetrg.name
   tags = {
     owner = "peter.colley"
   }
 }
 
 
-resource "azurerm_subnet" "subnet1" {
-  name                 = "subnet1"
-  resource_group_name = azurerm_resource_group.vnetRG.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.21.22.0/24"]  # Set your desired subnet address range
+# SUBNETS #
+
+# resource "azurerm_subnet" "subnet1" {
+#   name                  = "subnet1"
+#   resource_group_name   = azurerm_resource_group.vnetrg.name
+#   virtual_network_name  = azurerm_virtual_network.vnet.name
+#   address_prefixes      = ["10.21.22.0/24"]  # Set your desired subnet address range
+# }
+
+# resource "azurerm_subnet" "subnet2" {
+#   name                  = "subnet2"
+#   resource_group_name   = azurerm_resource_group.vnetrg.name
+#   virtual_network_name  = azurerm_virtual_network.vnet.name
+#   address_prefixes      = ["10.21.23.0/24"]  # Set your desired subnet address range
+# }
+
+
+# LOAD BALANCER
+
+resource "azurerm_public_ip" "azlb" {
+  allocation_method       = var.allocation_method
+  location                = azurerm_resource_group.vnetrg.location
+  name                    = local.pip_name
+  resource_group_name     = azurerm_resource_group.vnetrg.name
+  ddos_protection_mode    = var.pip_ddos_protection_mode
+  ddos_protection_plan_id = var.pip_ddos_protection_plan_id
+  domain_name_label       = var.pip_domain_name_label
+  edge_zone               = var.edge_zone
+  idle_timeout_in_minutes = var.pip_idle_timeout_in_minutes
+  ip_tags                 = var.pip_ip_tags
+  ip_version              = var.pip_ip_version
+  public_ip_prefix_id     = var.pip_public_ip_prefix_id
+  reverse_fqdn            = var.pip_reverse_fqdn
+  sku                     = var.pip_sku
+  sku_tier                = var.pip_sku_tier
+  zones                   = var.pip_zones
 }
 
-resource "azurerm_subnet" "subnet2" {
-  name                 = "subnet2"
-  resource_group_name = azurerm_resource_group.vnetRG.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.21.23.0/24"]  # Set your desired subnet address range
+resource "azurerm_lb" "lb1" {
+  location            = azurerm_resource_group.vnetrg.location
+  resource_group_name = azurerm_resource_group.vnetrg.name
+  name                = local.lb_name
+  edge_zone           = var.lb_edge_zone
+  sku                 = var.lb_sku
+  sku_tier            = var.lb_sku_tier
+
+  tags = {
+    owner = "peter.colley"
+  }
+
+  # frontend_ip_configuration {
+  #   name                          = var.frontend_name
+  #   private_ip_address            = var.frontend_private_ip_address
+  #   private_ip_address_allocation = var.frontend_private_ip_address_allocation
+  #   private_ip_address_version    = var.frontend_private_ip_address_version
+  #   public_ip_address_id          = 
+  #   subnet_id                     = azurerm_subnet.subnet1.id
+  #   zones                         = var.frontend_ip_zones
+  # }
+
+  # lifecycle {
+  #   precondition {
+  #     condition     = var.frontend_subnet_name == null || var.frontend_subnet_name == "" || var.frontend_subnet_id == null || var.frontend_subnet_id == ""
+  #     error_message = "frontend_subnet_name or frontend_vent_name cannot exist if frontend_subnet_id exists."
+  #   }
+  # }
+}
+
+
+
+
+
+# PALO ALTO PAN-OS
+
+resource "azurerm_palo_alto_next_generation_firewall_virtual_network_panorama" "panos1" {
+  name                    = "TestPanosFirewall"
+  resource_group_name     = azurerm_resource_group.vnetrg.name
+  location                = azurerm_resource_group.vnetrg.location
+  panorama_base64_config  = "e2RnbmFtZTogY25nZnctYXotZXhhbXBsZSwgdHBsbmFtZTogY25nZnctZXhhbXBsZS10ZW1wbGF0ZS1zdGFjaywgZXhhbXBsZS1wYW5vcmFtYS1zZXJ2ZXI6IDE5Mi4xNjguMC4xLCB2bS1hdXRoLWtleTogMDAwMDAwMDAwMDAwMDAwLCBleHBpcnk6IDIwMjQvMDcvMzF9Cg=="
+
+  network_profile {
+    public_ip_address_ids = [azurerm_public_ip.example.id]
+
+    vnet_configuration {
+      virtual_network_id  = azurerm_virtual_network.vnet.id
+      trusted_subnet_id   = azurerm_subnet.subnet1.id
+      untrusted_subnet_id = azurerm_subnet.subnet2.id
+    }
+  }
 }
--- main/variables.tf
+++ main/variables.tf
@@ -1,28 +1,176 @@
-#############################################################################
-# VARIABLES
-#############################################################################
-
+# GLOBAL
 variable "location" {
-  type    = string
-  default = "Australia East"
-}
-
-variable "vnet_location" {
-  type    = string
-  default = "Australia East"
+  type        = string
+  default     = "Australia East"
+  description = "Define location where resources will be deployed"
 }
 
 variable "naming_prefix" {
-  type    = string
-  default = "origintest"
-}
-
-variable "use_for_each" {
-  type    = bool
-  default = true
+  type        = string
+  default     = "origintest"
+  description = "Prefix for naming all deployed resources."
 }
 
 variable "tags" {
   type    = list(string)
   default = ["Environment:Dev", "CostCenter:IT"]
+}
+
+
+# VNET
+variable "use_for_each" {
+  type    = bool
+  default = true
+}
+
+
+# PANOS
+variable "pan_os_password" {
+    type = string
+    default = "$mytestpassword123"
+    sensitive = true
+    description = "Password for the palo alto panorama firewall."
+}
+
+
+# PUBLIC IP
+variable "allocation_method" {
+  type        = string
+  default     = "Static"
+  description = "(Required) Defines how an IP address is assigned. Options are Static or Dynamic."
+}
+
+variable "pip_ddos_protection_mode" {
+  type        = string
+  default     = "VirtualNetworkInherited"
+  description = "(Optional) The DDoS protection mode of the public IP. Possible values are `Disabled`, `Enabled`, and `VirtualNetworkInherited`. Defaults to `VirtualNetworkInherited`."
+}
+
+variable "pip_ddos_protection_plan_id" {
+  type        = string
+  default     = null
+  description = "(Optional) The ID of DDoS protection plan associated with the public IP. `ddos_protection_plan_id` can only be set when `ddos_protection_mode` is `Enabled`."
+}
+
+variable "pip_domain_name_label" {
+  type        = string
+  default     = null
+  description = "(Optional) Label for the Domain Name. Will be used to make up the FQDN.  If a domain name label is specified, an A DNS record is created for the public IP in the Microsoft Azure DNS system."
+}
+
+variable "pip_idle_timeout_in_minutes" {
+  type        = number
+  default     = 4
+  description = "(Optional) Specifies the timeout for the TCP idle connection. The value can be set between 4 and 30 minutes. Defaults to `4`."
+}
+
+variable "pip_ip_tags" {
+  type        = map(string)
+  default     = null
+  description = "(Optional) A mapping of IP tags to assign to the public IP. Changing this forces a new resource to be created. IP Tag `RoutingPreference` requires multiple `zones` and `Standard` SKU to be set."
+}
+
+variable "pip_ip_version" {
+  type        = string
+  default     = "IPv4"
+  description = "(Optional) The IP Version to use, `IPv6` or `IPv4`. Defaults to `IPv4`. Changing this forces a new resource to be created. Only `static` IP address allocation is supported for `IPv6`."
+}
+
+variable "pip_name" {
+  type        = string
+  default     = ""
+  description = "(Optional) Name of public ip. If it is set, the 'prefix' variable will be ignored."
+}
+
+variable "pip_public_ip_prefix_id" {
+  type        = string
+  default     = null
+  description = "(Optional) If specified then public IP address allocated will be provided from the public IP prefix resource. Changing this forces a new resource to be created."
+}
+
+variable "pip_reverse_fqdn" {
+  type        = string
+  default     = null
+  description = "(Optional) A fully qualified domain name that resolves to this public IP address. If the reverseFqdn is specified, then a PTR DNS record is created pointing from the IP address in the in-addr.arpa domain to the reverse FQDN."
+}
+
+variable "pip_sku" {
+  type        = string
+  default     = "Basic"
+  description = "(Optional) The SKU of the Azure Public IP. Accepted values are Basic and Standard."
+}
+
+variable "pip_sku_tier" {
+  type        = string
+  default     = "Regional"
+  description = "(Optional) The SKU Tier that should be used for the Public IP. Possible values are `Regional` and `Global`. Defaults to `Regional`. Changing this forces a new resource to be created."
+}
+
+variable "pip_zones" {
+  type        = list(string)
+  default     = null
+  description = "(Optional) A collection containing the availability zone to allocate the Public IP in. Changing this forces a new resource to be created. Availability Zones are only supported with a [Standard SKU](https://docs.microsoft.com/azure/virtual-network/virtual-network-ip-addresses-overview-arm#standard) and [in select regions](https://docs.microsoft.com/azure/availability-zones/az-overview) at this time. Standard SKU Public IP Addresses that do not specify a zone are **not** zone-redundant by default."
+}
+
+
+# LOAD BALANCER
+variable "edge_zone" {
+  type        = string
+  default     = null
+  description = "(Optional) Specifies the Edge Zone within the Azure Region where this Public IP and Load Balancer should exist. Changing this forces new resources to be created."
+}
+
+variable "lb_sku" {
+  type        = string
+  default     = "Basic"
+  description = "(Optional) The SKU of the Azure Load Balancer. Accepted values are Basic and Standard."
+}
+
+variable "lb_sku_tier" {
+  type        = string
+  default     = "Regional"
+  description = "(Optional) The SKU tier of this Load Balancer. Possible values are `Global` and `Regional`. Defaults to `Regional`. Changing this forces a new resource to be created."
+}
+
+variable "tracing_tags_enabled" {
+  type        = bool
+  default     = false
+  description = "Whether enable tracing tags that generated by BridgeCrew Yor."
+  nullable    = false
+}
+
+variable "type" {
+  type        = string
+  default     = "public"
+  description = "(Optional) Defined if the loadbalancer is private or public"
+}
+
+variable "frontend_ip_zones" {
+  type        = set(string)
+  default     = null
+  description = "(Optional) A collection containing the availability zone to allocate the IP in. Changing this forces a new resource to be created. Availability Zones are only supported with a [Standard SKU](https://docs.microsoft.com/azure/virtual-network/virtual-network-ip-addresses-overview-arm#standard) and [in select regions](https://docs.microsoft.com/azure/availability-zones/az-overview) at this time. Standard SKU Public IP Addresses that do not specify a zone are **not** zone-redundant by default."
+}
+
+variable "frontend_name" {
+  type        = string
+  default     = "myPublicIP"
+  description = "(Required) Specifies the name of the frontend ip configuration."
+}
+
+variable "frontend_private_ip_address" {
+  type        = string
+  default     = ""
+  description = "(Optional) Private ip address to assign to frontend. Use it with type = private"
+}
+
+variable "frontend_private_ip_address_allocation" {
+  type        = string
+  default     = "Dynamic"
+  description = "(Optional) Frontend ip allocation type (Static or Dynamic)"
+}
+
+variable "frontend_private_ip_address_version" {
+  type        = string
+  default     = null
+  description = "(Optional) The version of IP that the Private IP Address is. Possible values are `IPv4` or `IPv6`."
 }
