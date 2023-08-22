75d250875a0f26ba8d1f89daa03d09fd6b0f27ad
Peter Colley <Peter.Colley@vigilant.it>
2023-08-23 09:53:43 +1000

    fixed public ip ref in panos1

--- main/main.tf
+++ main/main.tf
@@ -159,7 +159,7 @@ resource "azurerm_palo_alto_next_generation_firewall_virtual_network_panorama" "
   panorama_base64_config  = "e2RnbmFtZTogY25nZnctYXotZXhhbXBsZSwgdHBsbmFtZTogY25nZnctZXhhbXBsZS10ZW1wbGF0ZS1zdGFjaywgZXhhbXBsZS1wYW5vcmFtYS1zZXJ2ZXI6IDE5Mi4xNjguMC4xLCB2bS1hdXRoLWtleTogMDAwMDAwMDAwMDAwMDAwLCBleHBpcnk6IDIwMjQvMDcvMzF9Cg=="
 
   network_profile {
-    public_ip_address_ids = [azurerm_public_ip.example.id]
+    public_ip_address_ids = [azurerm_public_ip.azlb.id]
 
     vnet_configuration {
       virtual_network_id  = azurerm_virtual_network.vnet.id
