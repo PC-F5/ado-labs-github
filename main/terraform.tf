1552e11806f984c6426a7f8511bb0ea741f2067b
Peter Colley <Peter.Colley@vigilant.it>
2023-08-23 09:35:30 +1000

    revert to initial required hashicorp/azurerm  version

--- main/terraform.tf
+++ main/terraform.tf
@@ -3,11 +3,18 @@
 ##################################################################################
 terraform {
   required_providers {
+    #
+    # this version is required for the palo alto module
+    # but it's going to require that the init has the -upgrade arguement
+    #
+    # azurerm = {
+    #   source  = "hashicorp/azurerm"
+    #   version = "=3.70.0"    
+    # }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.70.0"
+      version = "~> 2.0"
     }
-
   }
   backend "azurerm" {
     key = "app.terraform.tfstate"
