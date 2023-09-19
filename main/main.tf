cc5eddbef1a033279cd2da3d4debb7981c70ef86
Peter Colley <Peter.Colley@vigilant.it>
2023-09-19 14:03:09 +1000

    testing

--- main/main.tf
+++ main/main.tf
@@ -195,16 +195,16 @@ resource "azurerm_lb" "lb1" {
 # }
 
 
-# get a ref to our existing vnet
-data "azurerm_virtual_network" "existing_vnet" {
-  name                = "TestVnet"
-  resource_group_name = "origintest-17493"
-}
+# # get a ref to our existing vnet
+# data "azurerm_virtual_network" "existing_vnet" {
+#   name                = "TestVnet"
+#   resource_group_name = "origintest-17493"
+# }
 
-# deploy subnet to our discovered vnet
-resource "azurerm_subnet" "subnet3" {
-  name                 = "subnet3"
-  resource_group_name  = data.azurerm_virtual_network.existing_vnet.resource_group_name
-  virtual_network_name = data.azurerm_virtual_network.existing_vnet.name
-  address_prefixes     = ["99.0.3.0/24"] # Set your desired subnet address range
-}
+# # deploy subnet to our discovered vnet
+# resource "azurerm_subnet" "subnet3" {
+#   name                 = "subnet3"
+#   resource_group_name  = data.azurerm_virtual_network.existing_vnet.resource_group_name
+#   virtual_network_name = data.azurerm_virtual_network.existing_vnet.name
+#   address_prefixes     = ["99.0.3.0/24"] # Set your desired subnet address range
+# }
